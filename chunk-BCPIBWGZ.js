80ee79779374726551c4a686b49837b48255a5bc
GustavoOrozco <gustavo.orozcom68@gmail.com>
2026-03-28 17:05:37 -0500

    Auto-generated commit

---
+++ chunk-BCPIBWGZ.js
@@ -0,0 +1,1 @@
+var e={production:!1,apiURL:"https://customermanagement.top/api/",name:"development"};export{e as a};
--- chunk-JM3OMOB7.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,apiURL:"https://34.230.90.166/api/",name:"development"};export{e as a};
